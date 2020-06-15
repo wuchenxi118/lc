9ca1553bb3e5b6ad7195d8a2379e3cffb6f8c7b8
wuchenxi118 <wuchenxi118@126.com>
2020-06-16 00:56:26 +0800

    a

--- num139.py
+++ num139.py
@@ -1,5 +1,11 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict):
+    def wordBreak(self, s, wordDict):
+        n=len(s)
+        dp=[False]*(n+1)
+        dp[0]=True
+        for i in range(n):
+            for j in range(i+1,n+1):
+                if(dp[i] and (s[i:j] in wordDict)):
+                    dp[j]=True
+        return dp[-1]
 
-        for i in wordDict:
-            if i in 
