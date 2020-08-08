39d765c4580fcb0d411c485213232bb404447f11
wuchenxi118 <wuchenxi118@126.com>
2020-08-08 17:01:26 +0800

    end

--- num253_2.py
+++ num253_2.py
@@ -10,3 +10,19 @@ class Solution:
         last_time.sort()
         begin_point = 0
         end_point = 0
+        used_room = 0
+        while(begin_point<len(first_time)):
+            if first_time[begin_point]<last_time[end_point]:
+                used_room+=1
+            else:
+                end_point+=1
+            begin_point += 1
+        return used_room
+
+
+
+
+if __name__ == '__main__':
+    inp = [[0, 30],[5, 10],[15, 20],[16,30]]
+    S = Solution()
+    print(S.minMeetingRooms(inp))
